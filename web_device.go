4a9078584d1840734227b67bbb5ad78ae8183c13
kshitij korde <kshitij.korde@isode.com>
2021-04-26 16:42:25 +0100

    Modified function name and device parameter names

--- web_device.go
+++ web_device.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
@@ -9,21 +11,15 @@ import (
 	"regexp"
 )
 
-type Page struct {
-	Title                  string
+type DeviceStatus struct {
+	// Device name
+	Name string
+	// Status parameters
 	VSWR                   []byte
 	PowerSupplyVoltage     []byte
 	PowerSupplyConsumption []byte
 	Temperature            []byte
 	SignalLevel            []byte
-}
-
-func FileExists(filename string) bool {
-	info, err := os.Stat(filename)
-	if os.IsNotExist(err) {
-		return false
-	}
-	return !info.IsDir()
 }
 
 func check(e error) {
@@ -32,8 +28,8 @@ func check(e error) {
 	}
 }
 
-func (p *Page) save() error {
-	filename := p.Title + ".txt"
+func (p *DeviceStatus) save() error {
+	filename := p.Name + ".txt"
 
 	f, err := os.Create(filename)
 	check(err)
@@ -53,9 +49,9 @@ func (p *Page) save() error {
 	return err
 }
 
-func loadPage(title string) (*Page, error) {
+func loadDeviceStatus(name string) (*DeviceStatus, error) {
 
-	filename := title + ".txt"
+	filename := name + ".txt"
 	file, err := os.Open(filename)
 
 	if err != nil {
@@ -94,29 +90,29 @@ func loadPage(title string) (*Page, error) {
 			}
 		}
 	}
-	return &Page{Title: title, VSWR: _VSWR, PowerSupplyVoltage: _PowerSupplyVoltage,
+	return &DeviceStatus{Name: name, VSWR: _VSWR, PowerSupplyVoltage: _PowerSupplyVoltage,
 		PowerSupplyConsumption: _PowerSupplyConsumption, Temperature: _Temperature,
 		SignalLevel: _SignalLevel}, nil
 }
 
-func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
-	p, err := loadPage(title)
+func ViewHandler(w http.ResponseWriter, r *http.Request, name string) {
+	p, err := loadDeviceStatus(name)
 	if err != nil {
-		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		http.Redirect(w, r, "/edit/"+name, http.StatusFound)
 		return
 	}
-	renderTemplate(w, "view", p)
+	RenderTemplate(w, "view", p)
 }
 
-func editHandler(w http.ResponseWriter, r *http.Request, title string) {
-	p, err := loadPage(title)
+func EditHandler(w http.ResponseWriter, r *http.Request, name string) {
+	p, err := loadDeviceStatus(name)
 	if err != nil {
-		p = &Page{Title: title}
+		p = &DeviceStatus{Name: name}
 	}
-	renderTemplate(w, "edit", p)
+	RenderTemplate(w, "edit", p)
 }
 
-func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
+func SaveHandler(w http.ResponseWriter, r *http.Request, name string) {
 
 	_VSWR := r.FormValue("VSWR")
 	_PowerSupplyVoltage := r.FormValue("PowerSupplyVoltage")
@@ -124,23 +120,24 @@ func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
 	_Temperature := r.FormValue("Temperature")
 	_SignalLevel := r.FormValue("SignalLevel")
 
-	p := &Page{Title: title,
+	p := &DeviceStatus{Name: name,
 		VSWR:                   []byte(_VSWR),
 		PowerSupplyVoltage:     []byte(_PowerSupplyVoltage),
 		PowerSupplyConsumption: []byte(_PowerSupplyConsumption),
 		Temperature:            []byte(_Temperature),
 		SignalLevel:            []byte(_SignalLevel)}
+
 	err := p.save()
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+	http.Redirect(w, r, "/view/"+name, http.StatusFound)
 }
 
 var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
 
-func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+func RenderTemplate(w http.ResponseWriter, tmpl string, p *DeviceStatus) {
 	err := templates.ExecuteTemplate(w, tmpl+".html", p)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -149,7 +146,7 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 
 var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
 
-func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+func MakeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		m := validPath.FindStringSubmatch(r.URL.Path)
 		if m == nil {
@@ -160,11 +157,21 @@ func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.Handl
 	}
 }
 
+func ReturnStatus(w http.ResponseWriter, r *http.Request) {
+	device_status, err := loadDeviceStatus("radio")
+	fmt.Println(device_status)
+	if err == nil {
+		status_data, err := json.Marshal(device_status)
+		check(err)
+		fmt.Println(string(status_data))
+	}
+}
+
 func main() {
 
-	http.HandleFunc("/view/", makeHandler(viewHandler))
-	http.HandleFunc("/edit/", makeHandler(editHandler))
-	http.HandleFunc("/save/", makeHandler(saveHandler))
-
+	http.HandleFunc("/view/", MakeHandler(ViewHandler))
+	http.HandleFunc("/edit/", MakeHandler(EditHandler))
+	http.HandleFunc("/save/", MakeHandler(SaveHandler))
+	http.HandleFunc("/status", ReturnStatus)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
